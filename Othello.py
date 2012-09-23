2075e78675095871d4a44265fec6fa66b99e71dc
pelmers <peter.elmers@yahoo.com>
2012-09-23 14:24:55 -0500

    Initial commit

---
+++ Othello.py
@@ -0,0 +1,294 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+"""
+Othello board game, written as part of extended essay
+Peter Elmers
+"""
+
+import OthelloAI as ai
+import sys, time
+
+# screen width for progress bar, assumed 80
+WID = 80
+
+class GameBoard(object):
+    def __init__(self,white_char='O',black_char='X',white_source="human", black_source="human",starting_board="default"):
+        self.BORDER=2
+        self.EMPTY=0
+        self.WHITE=1
+        self.BLACK=-1
+        # generate board as list of 100 integers, 8x8 enclosed in 10x10 with borders
+        self.board=[self.EMPTY for i in range(100)]
+        for i in range(100):
+                if (str(i)[-1] == "0" or str(i)[-1] == "9" or i<10 or i>89):
+                    self.board[i] = self.BORDER
+        self.board[44] = self.board[55] = self.WHITE
+        self.board[45] = self.board[54] = self.BLACK
+        self.white_char = white_char
+        self.black_char = black_char
+        self.white_source = white_source
+        self.black_source = black_source
+        # start AI if it is playing
+        if self.white_source != "human":
+            self.ai_white = ai.OthelloAI(self,self.WHITE,self.white_source,starting_board)
+        if self.black_source != "human":
+            self.ai_black = ai.OthelloAI(self,self.BLACK,self.black_source,starting_board)
+        self.directions = [1,-1,10,-10,9,-9,11,-11]
+        self.side= self.BLACK 
+        self.unplayed = 0
+        self.last_move = "[no move played yet]"
+
+    def repr_board(self):
+        """
+        Return a string representing the current board position.
+        """
+        output = '\t'
+        for index,value in enumerate(self.board):
+            if 1<=index<=8:
+                output += str(index)+' '*3
+            elif index%10==0 and index!=0 and index!=90:
+                output+=str(index)+'\t'
+            elif str(index)[-1] == '9' and index<89:
+                output += '\n'+'-'*40+'\n'
+            else:
+                if value == self.WHITE:
+                    output += self.white_char+' '*3
+                elif value == self.BLACK:
+                    output += self.black_char+' '*3
+                elif value == self.EMPTY:
+                    output += '-'+' '*3
+        scores = self.find_victor()
+        output += "\nCurrent score:\nBlack: %s\nWhite: %s" % (scores[2], scores[1])
+        return output+'\nLast move was: %s' % (self.last_move)
+    
+    def flipped_squares(self, move_pos, side):
+        """
+        Return a list of positions that would be flipped by a tile played at move_pos.
+        """
+        to_flip = []
+        for direction in self.directions:
+            next_pos = move_pos + direction
+            if self.board[next_pos] == -side:
+                repeating = True
+                flip_this_dir = [next_pos]
+                while repeating:
+                    next_pos += direction
+                    if (self.board[next_pos] == self.EMPTY) or (self.board[next_pos] == self.BORDER):
+                        break
+                    if self.board[next_pos] == -side:
+                        flip_this_dir.append(next_pos)
+                        continue
+                    elif self.board[next_pos] == side:
+                        to_flip += flip_this_dir
+                        break
+        return to_flip
+
+    def legal_move(self, move_pos, side):
+        """
+        Return False if move is not legal.
+        Return list of tiles to flip if legal.
+        """
+        if self.board[move_pos] != self.EMPTY:
+            return False
+        else:
+            flipped = self.flipped_squares(move_pos, side)
+            if len(flipped) == 0:
+                return False
+            else:
+                 return flipped
+    
+    def make_move(self, move_pos, side):
+        """
+        Try to make a move on the current board.
+        Return False if move is illegal.
+        If move is legal, return True and make the move.
+        """
+        to_flip = self.legal_move(move_pos, side)
+        if to_flip == False:
+            return False
+        for pos in to_flip:
+            if self.board[pos] == self.WHITE:
+                self.board[pos] = self.BLACK
+            else:
+                self.board[pos] = self.WHITE
+        self.board[move_pos] = side
+        return True
+
+    def get_move(self, side, source="human"):
+        """
+        Return a move by querying the appropriate source.
+        """
+        if source == "human":
+            while 1:
+                possible_moves = []
+                for pos in range(11,89):
+                    if self.legal_move(pos, side):
+                        possible_moves.append(str(pos))
+                print "Possible moves: %s" % (' '.join(possible_moves))
+                try:
+                    move = int(raw_input("Enter your move (sum of row and column): "))
+                    if str(move) not in possible_moves:
+                        print "Invalid move, please try again."
+                        continue
+                except ValueError:
+                    print "Invalid move, please try again."
+                    continue
+                break
+        else:
+            if side == self.WHITE:
+                move = self.ai_white.find_move()
+            if side == self.BLACK:
+                move = self.ai_black.find_move()
+        return move
+    
+    def test_possible_moves(self, side):
+        """
+        Return True if the side has any possible moves.
+        If no moves are possible, return False.
+        """
+        for pos in range(11,89):
+            if self.legal_move(pos, side):
+                return True
+        return False
+
+    def test_end(self):
+        """
+        Return True if game has ended, else False.
+        """
+        if self.unplayed == 2:
+            return True
+        if sum([1 for i in range(11,89) if self.board[i] == self.EMPTY]) == 0:
+            return True
+        return False
+        
+    
+    def play_turn(self,show=True):
+        """
+        Handle getting the move from the players, making the move, and going to the next turn.
+        Return True if game continues, False if it ends
+        """
+        if show:
+            print self
+        if self.test_end() == True:
+            return False
+        if self.test_possible_moves(self.side) == False:
+            self.unplayed += 1
+            self.side = -self.side
+            return True
+        if self.side == self.WHITE:
+            move = self.get_move(self.side, self.white_source)
+            self.last_move = move
+            self.make_move(move,self.side)
+        elif self.side == self.BLACK:
+            move = self.get_move(self.side, self.black_source)
+            self.last_move = move
+            self.make_move(move,self.side)
+        self.side = -self.side
+        self.unplayed = 0
+        return True
+
+    
+    def find_victor(self):
+        """
+        Find the winner and scores of each player of the game.
+        Assume that the game is finished.
+        """
+        white_count = sum([1 for i in range(11,89) if (self.board[i]==self.WHITE)])
+        black_count = sum([1 for i in range(11,89) if (self.board[i]==self.BLACK)])
+        if white_count > black_count:
+            return self.WHITE, white_count, black_count
+        elif black_count > white_count:
+            return self.BLACK, white_count, black_count
+        elif white_count == black_count:
+            return self.EMPTY, white_count, black_count
+
+    def __str__(self):
+        # lets us do 'print self' to simplify showing board
+        return self.repr_board()
+
+def progress_bar(width, percent, char='#'): # to show simulation progress
+    """
+    Progress bar with variable width, scales percentage to width
+    Example: [ ####------ ] 42%
+    """
+    if width < 10:
+        return 'bad width'
+    width += -7
+    width += -len(str(percent))
+    filled = int(round((float(width)*(float(percent)/100))))
+    return '\r[ %s%s ] %i ' % (
+    char[0:1]*filled, '-'*(width-filled), percent) + r'%'
+
+def menu(choices_list,message):
+    choices_dict = dict(enumerate(choices_list))
+    for k,v in choices_dict.items():
+        print "%s) %s" % (k+1,v)
+    choice = choices_dict[int(raw_input("%s" % (message)))-1]
+    print "%s selected" % (choice)
+    return choice
+
+
+def main():
+    sources = ['human','random','shallow','minimax','ab']
+    black_source = menu(sources,"Source for black player: ")
+    white_source = menu(sources,"Source for white player: ")
+    sim_number = 0
+    if white_source != "human" and black_source != "human":
+        if raw_input("Do you want to simulate games? [Y/n] ") in ['y','Y','yes',"Yes"]:
+            sim_number = int(raw_input("How many games to simulate? "))
+            randomize = raw_input("Do you want to randomize game starts? ")
+            if randomize in ['y','Y','yes','Yes']:
+                randomize = "random"
+            else:
+                randomize = "default"
+            output = '\n'
+    if sim_number == 0:
+        # non-simulation portion of main()
+        game = GameBoard(white_source=white_source, black_source=black_source)    
+        Playing = True
+        while Playing:
+            Playing = game.play_turn()
+        victor,whites,blacks = game.find_victor()
+        if victor == game.WHITE:
+            print "White has won with a score of %s to %s" % (whites, blacks)
+        if victor == game.BLACK:
+            print "Black has won with a score of %s to %s" % (blacks, whites)
+        if victor == game.EMPTY:
+            print "The game is tied with a score of %s to %s" % (whites, blacks)
+    else:
+        # simulation processing and output
+        percent = None
+        black_wins = 0
+        white_wins = 0
+        draws = 0
+        Start = time.clock()
+        # ready, set, go!
+        for sim in range(sim_number):
+            new_percent = round(float(sim)/float(sim_number),2)*100
+            if new_percent != percent or percent is None:
+               sys.stdout.write(progress_bar(WID, new_percent)) 
+               sys.stdout.flush()
+               percent = new_percent
+            game = GameBoard(white_source=white_source, black_source=black_source,starting_board=randomize)
+            Playing = True
+            while Playing:
+                Playing = game.play_turn(show=False)
+            victor, whites, blacks = game.find_victor()
+            if victor == game.WHITE:
+                output += "White wins %s to %s \n" % (whites, blacks)
+                white_wins += 1
+            elif victor == game.BLACK:
+                output += "Black wins %s to %s \n" % (blacks, whites)
+                black_wins += 1
+            else:
+                output += "Game tied %s to %s \n" % (whites, blacks)
+                draws += 1
+        End = time.clock()
+        print output
+        print "Black %s\nWhite %s\nDraw %s" % (str(round(float(black_wins)/float(sim_number),3)*100)+r'%',
+            str(round(float(white_wins)/float(sim_number),3)*100)+r'%', str(round(float(draws)/float(sim_number),3)*100)+r'%')
+        print "Simulation lasted %.3f seconds, %.3f seconds per game." % ((End-Start),(End-Start)/sim_number)
+
+
+if __name__ == '__main__':
+    main()
---
+++ OthelloAI.py
@@ -0,0 +1,227 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+"""
+AI component of Othello board game
+Peter Elmers
+"""
+
+import random, sys
+
+class OthelloAI(object):
+    """
+    OthelloAI
+    """
+    def __init__(self, gameObject, side, strat="random",start="default"):
+        self.game = gameObject
+        self.side = side
+        self.strat = strat
+        self.board_start = start
+        self.move_count = 0
+        # some positions are better, some worse
+        self.weights = [
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,120,-20, 20,  5,  5, 20,-20,120,  0,
+        0,-20,-40, -5, -5, -5, -5,-40,-20,  0,
+        0, 20, -5,  3,  3,  3,  3, -5, 20,  0,
+        0,  5, -5,  3,  3,  3,  3, -5,  5,  0,
+        0,  5, -5,  3,  3,  3,  3, -5,  5,  0,
+        0, 20, -5,  3,  3,  3,  3, -5, 20,  0,
+        0,-20,-40, -5, -5, -5, -5,-40,-20,  0,
+        0,120,-20, 20,  5,  5, 20,-20,120,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0]
+
+        # lookup in dicts is faster
+        self.weights = dict((k,v) for k,v in enumerate(self.weights))
+
+        #self.weights = dict((pos,5) for pos in range(11,89))
+        #for i in [11,18,81,88]:
+        #    self.weights[i] = 120
+        #for i in [12,22,21,17,27,28,71,72,82,87,77,78]:
+        #    self.weights[i] = -50
+        #for i in [13,14,15,16,31,41,51,61,38,48,58,68,83,84,85,86]:
+        #    self.weights[i] = 25
+    
+    def random_strat(self):
+        """
+        Find a move by randomly choosing from all possibilities.
+        """
+        possible_moves = []
+        for pos in range(11,89):
+            if self.game.legal_move(pos, self.side):
+                possible_moves.append(pos)
+        return random.choice(possible_moves) 
+
+    def evaluate_state(self,side):
+        """
+        Return a score that evaluates the state of the board.
+        Uses side to determine which side's point of view to take.
+        """
+        score = 0
+        if self.game.test_end():
+            if self.game.find_victor()[0] == side:
+                return 9999999 # positive infinity
+            elif self.game.find_victor()[0] == -side:
+                return -9999999 # negative infinity
+        for pos in range(11,89):
+            if self.game.board[pos] == side:
+                score += self.weights[pos]
+            elif self.game.board[pos] == -side:
+                score -= self.weights[pos]
+        return score
+
+    def shallow_search(self):
+        """
+        Return a move that results in the best outcome immediately following it.
+        """
+        original_board = self.game.board[:]
+        max_score = False
+        scores = dict((pos,None) for pos in range(11,89))
+        for pos in range(11,89):
+           if self.game.make_move(pos,self.side)==False:
+               continue
+           else:
+               score = self.evaluate_state(self.side)
+               scores[pos]=score
+               self.game.board = original_board[:]
+        for pos, score in scores.iteritems():
+           if score != None:
+               if max_score == False:
+                   max_score = score
+                   move = pos
+               if score > max_score:
+                   move = pos
+                   max_score = score
+        return move
+
+    def maximize(self,ply,side):
+        """
+        Return the maximum score possible from the current position.
+        Use for playing side's move.
+        """
+        if ply == 0 or self.game.test_end():
+            return self.evaluate_state(side) 
+        score = -9999999999
+        current_board = self.game.board[:]
+        for pos in range(11,89):
+            if self.game.make_move(pos,side) == False:
+                continue
+            self.game.side = side
+            score = max(score,self.minimize(ply-1,-side))
+            self.game.board = current_board[:]
+        return score
+
+    def minimize(self,ply,side):
+        """
+        Return the lowest score possible from this position.
+        Use for opponent's move.
+        """
+        if ply == 0 or self.game.test_end():
+            return self.evaluate_state(side)
+        score = 99999999999
+        current_board = self.game.board[:]
+        for pos in range(11,89):
+            if self.game.make_move(pos,side) == False: 
+                continue
+            self.game.side = side
+            score = min(score,self.maximize(ply-1,-side))
+            self.game.board = current_board[:]
+        return score
+
+    def minimax_search(self,maxply):
+        """
+        Return a move by using a minimax search to maxply levels deep.
+        """
+        current_board = self.game.board[:]
+        best_score = None
+        for pos in range(11,89):
+            if self.game.make_move(pos,self.game.side) == False:
+                continue
+            result_score = self.minimize(maxply,-self.game.side)
+            self.game.board = current_board[:]
+            self.game.side = self.side
+            if best_score is None or result_score > best_score:
+                best_score = result_score 
+                best_move = pos
+        return best_move
+
+    def ab_maximize(self,ply,side,alpha,beta):
+        """
+        Return the maximum score possible from the current position.
+        Use for playing side's move.
+        """
+        if ply == 0 or self.game.test_end():
+            return self.evaluate_state(side)
+        current_board = self.game.board[:]
+        for pos in range(11,89):
+            if self.game.make_move(pos,side) == False:
+                continue
+            self.game.side = side
+            alpha = max(alpha,self.ab_minimize(ply-1,-side,alpha,beta))
+            self.game.board = current_board[:]
+            if beta <= alpha:
+                break
+        return alpha
+
+    def ab_minimize(self,ply,side,alpha,beta):
+        """
+        Return the lowest score possible from this position.
+        Use for opponent's move.
+        """
+        if ply == 0 or self.game.test_end():
+            return self.evaluate_state(side)
+        current_board = self.game.board[:]
+        for pos in range(11,89):
+            if self.game.make_move(pos,side) == False: 
+                continue
+            self.game.side = side
+            beta = min(beta,self.ab_maximize(ply-1,-side,alpha,beta))
+            self.game.board = current_board[:]
+            if beta <= alpha:
+                break
+        return beta
+
+    def alphabeta_search(self,maxply):
+        """
+        Return a move by using a minimax search to maxply levels deep.
+        """
+        current_board = self.game.board[:]
+        best_score = None
+        for pos in range(11,89):
+            if self.game.make_move(pos,self.game.side) == False:
+                continue
+            result_score = self.ab_minimize(maxply,-self.game.side,-9999,9999)
+            self.game.board = current_board[:]
+            self.game.side = self.side
+            if best_score is None or result_score > best_score:
+                best_score = result_score 
+                best_move = pos
+        return best_move
+
+    def find_move(self):
+        """
+        Return a move by implementing a strategy determined by the attribute self.strat
+        First three moves are random if self.board_start is set
+        """
+        self.move_count+=1
+        if self.board_start == "random" and self.move_count <= 5:
+            return self.random_strat()
+        if self.strat == "random":
+            return self.random_strat()
+        elif self.strat == "shallow":
+            return self.shallow_search()
+        elif self.strat == "minimax":
+            # count the number of empty squares
+            # return self.minimax_search(3)
+            emptys = sum([1 for i in range(11,89) if self.game.board[i] == self.game.EMPTY])
+            # return self.minimax_search(1)
+            if emptys < 8:
+                # search to the end of the game
+                return self.minimax_search(emptys)
+            #else:
+            return self.minimax_search(1)
+        elif self.strat == "ab":
+            emptys = sum([1 for i in range(11,89) if self.game.board[i] == self.game.EMPTY])
+            if emptys < 8:
+                return self.alphabeta_search(emptys)
+            #else:
+            return self.alphabeta_search(1)
